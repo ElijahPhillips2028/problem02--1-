ad43b95e93dbafde7f6c65fcbd48a54e44181b3f
Elijah Phillips <elp183@pitt.edu>
2026-02-07 16:07:08 -0500

    made a bad test file

--- lists/test_LinkedList.cpp
+++ lists/test_LinkedList.cpp
@@ -7,4 +7,20 @@
 TEST_CASE( "TODO", "[LinkedList]" ) {
 
     LinkedList<int> list;
+     
+  list.insert(0, 20);
+  REQUIRE(list.getEntry(0) == 20);
+  REQUIRE(list.getLength() == 1);
+  list.insert(1, 30);
+  REQUIRE(list.getEntry(1) == 30);
+  REQUIRE(list.getLength() == 2);
+  list.setEntry(0, 40);
+  REQUIRE(list.getEntry(0) == 40);
+  list.remove(0);
+  REQUIRE(list.getEntry(0) == 30);
+  REQUIRE(list.getLength() == 1);
+  list.clear();
+  REQUIRE(list.isEmpty() == true);
+  REQUIRE(list.getLength() == 0);
+
 }
